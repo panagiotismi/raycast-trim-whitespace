191deb55e55680d2098fe93770088a85f119a28c
Panagiotis Michalopoulos <panagiotis.michalopoulos@citrix.com>
2023-05-15 15:21:02 +0300

    update to latest api

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import { Clipboard } from "@raycast/api";
 
 const isString = (val: unknown) => typeof val === "string" || val instanceof String;
 
-export default async function main() {
-  const clipTxt = await Clipboard.readText();
-  if (clipTxt == null) {
+export default async function Command() {
+  const text = await Clipboard.readText();
+
+  if (text == null) {
     throw new Error("No text in clipboard");
   }
-  await Clipboard.paste(isString(clipTxt) ? clipTxt.trim() : clipTxt);
+  await Clipboard.paste(isString(text) ? text.trim() : text);
 }
